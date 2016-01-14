c50c0325d16ae5e3de0bea104ce537796fd43a21
Daniel Kolsoi <thadan64@gmail.com>
2016-01-13 21:45:48 -0500

    Added event loop, updated deps

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+#[macro_use] extern crate conrod;
+extern crate find_folder;
+extern crate piston_window;
+
+use conrod::{Labelable, Positionable, Sizeable, Colorable, Theme, Ui, Widget};
+use conrod::color;
+use piston_window::{EventLoop, Glyphs, PistonWindow, UpdateEvent, WindowSettings};
+
 fn main() {
-    println!("Hello, world!");
+    let window: PistonWindow = WindowSettings::new("Test window", [400, 270]).build().unwrap();
+
+    for event in window.ups(30) {
+    	println!("Hello, world!")
+    }
 }
